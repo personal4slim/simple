2419148915ee190adddff309a2667a176fed7fb1
personal4slim <129345989+personal4slim@users.noreply.github.com>
2023-08-28 14:18:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,20 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
