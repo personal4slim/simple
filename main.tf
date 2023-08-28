5dbe8320bd3bd2d375262c2482e6c6c4baaa27a9
personal4slim <129345989+personal4slim@users.noreply.github.com>
2023-08-28 09:34:11 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-Subscription_id = "606e824b-aaf7-4b4e-9057-b459f6a4436d"
-Secret_id = "7f7f47ce-e23b-4034-90da-0823fa454b18"
-client_id = "62ba6d2f-4192-4cf5-a00d-42328f7d7dfd"
-tenant_id = "104e77d4-81e7-4c16-ab44-935220bed6dd"
+subscription_id="606e824b-aaf7-4b4e-9057-b459f6a4436d"
+secret_id="7f7f47ce-e23b-4034-90da-0823fa454b18"
+client_id="62ba6d2f-4192-4cf5-a00d-42328f7d7dfd"
+tenant_id="104e77d4-81e7-4c16-ab44-935220bed6dd"
   features {}
 }
 
