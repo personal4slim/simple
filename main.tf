835841a527e8b4173991f7d7e9507fab4d36e997
personal4slim <129345989+personal4slim@users.noreply.github.com>
2023-08-28 10:55:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,6 @@ provider "azurerm" {
 #variable "subscription_id" {}
 #variable "client_id" {}
 #variable "tenant_id" {}
-subscription_id = "606e824b-aaf7-4b4e-9057-b459f6a4436d"
-client_id       = "62ba6d2f-4192-4cf5-a00d-42328f7d7dfd"
-tenant_id       = "104e77d4-81e7-4c16-ab44-935220bed6dd"
-secret_id       = "7f7f47ce-e23b-4034-90da-0823fa454b18"
 }
 resource "azurerm_resource_group" "example_rg" {
   name     = "my-resource-group"
