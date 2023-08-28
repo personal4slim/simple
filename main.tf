324b67ef045e42bb988d0c68678b152773bc3c1d
personal4slim <129345989+personal4slim@users.noreply.github.com>
2023-08-28 09:47:27 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 provider "azurerm" {
-subscription_id="606e824b-aaf7-4b4e-9057-b459f6a4436d"
-client_id="62ba6d2f-4192-4cf5-a00d-42328f7d7dfd"
-tenant_id="104e77d4-81e7-4c16-ab44-935220bed6dd"
   features {}
 }
+
+variable "subscription_id" {}
+variable "client_id" {}
+variable "tenant_id" {}
 
 resource "azurerm_resource_group" "example_rg" {
   name     = "my-resource-group"
